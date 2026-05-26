e6aa3fa88864e33e7293f410df209abfd238b0dc
João Pedro Caetano de Matos <112180592+caetanodemat@users.noreply.github.com>
2026-05-26 16:57:30 -0300

    loops_pratica1

---
+++ loops_pratica_1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php
+        
+        $registros = array(
+            array('Titulo' => 'Titulo noticia 1', 'Conteudo' => 'Conteudo noticia 1'),
+            array('Titulo' => 'Titulo noticia 2', 'Conteudo' => 'Conteudo noticia 2'),
+            array('Titulo' => 'Titulo noticia 3', 'Conteudo' => 'Conteudo noticia 3'),
+            array('Titulo' => 'Titulo noticia 4', 'Conteudo' => 'Conteudo noticia 4')
+        );
+
+        $idx = 0;
+        echo 'O array possui: '. count($registros) . ' registros';
+        echo '<hr/>';
+        while($idx < count($registros)){
+            echo '<h3>'.$registros[$idx]['Titulo'].'</h3>';
+            echo '<p>'.$registros[$idx]['Conteudo'].'</p>';
+            echo '<hr/>';
+            $idx++;
+        }
+
+        echo '<pre>';
+        print_r($registros);
+        echo '<prev/>';
+
+        do {
+            echo '<h3>'.$registros[$idx]['Titulo'].'</h3>';
+            echo '<p>'.$registros[$idx]['Conteudo'].'</p>';
+            echo '<hr/>';
+            $idx++;
+        
+        }while ($idx < count($registros));
+
+        
+
+
+        
+        
+        
+        ?>
+</body>
+</html>
