31a9cc2bc295350bb7b70e5fa07bdd0b73092bc0
João Pedro Caetano de Matos <112180592+caetanodemat@users.noreply.github.com>
2026-05-29 12:05:57 -0300

    valida_login

---
+++ App/app_help_desk/valida_login.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+
+    print_r($_SESSION);
+    echo '<hr />';
+    $usuario_autenticado = false;
+    $usuarios_app = array(
+        array('email' => 'joaopedrocaetanodematos@outlook.com', 'senha' => '123456'),
+        array('email' => 'user@helpdesk.com', 'senha' => '654321'),
+    );
+
+    foreach ($usuarios_app as $user) {
+        if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if ($usuario_autenticado) {
+        $_SESSION['autenticado'] = 'SIM';
+        echo 'Usuário autenticado com sucesso!';
+    } else {
+        $_SESSION['autenticado'] = 'NAO';
+        header('Location: index.php?login=erro');
+    }
+
+    /*print_r($_POST);
+
+    echo '<br />';
+    echo $_POST['email'];
+    echo '<br />';
+    echo $_POST['senha'];*/
+?>
